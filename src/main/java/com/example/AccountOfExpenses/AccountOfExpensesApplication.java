1a4a8251161c16a6c3180fa6957394c3710b14d1
Natsumi Nagasu <Natsumi.Nagasu@gmail.com>
2024-09-02 22:39:25 +0900

    first-messages

---
+++ src/main/java/com/example/AccountOfExpenses/AccountOfExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.example.AccountOfExpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountOfExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountOfExpensesApplication.class, args);
+	}
+
+}
